b464f53fc53254ee3c3913ac2d01ba73fe32431a
Leon Langhoff <leon.langhoff@posteo.de>
2022-01-15 22:24:35 +0100

    edit

--- compare_triplets.rb
+++ compare_triplets.rb
@@ -1,15 +1,16 @@
-a = gets.strip.split(' ').map(&:to_i)
-b = gets.strip.split(' ').map(&:to_i)
-c = a.zip(b)
+def compareTriplets(a, b)
+  a = gets.strip.split(' ').map(&:to_i)
+  b = gets.strip.split(' ').map(&:to_i)
+  c = a.zip(b)
 
-alice = 0
-bob = 0
-c.each do |a, b|
-  if a > b
-    alice += 1
-  elsif a < b
-    bob += 1
+  alice = 0
+  bob = 0
+  c.each do |a, b|
+    if a > b
+      alice += 1
+    elsif a < b
+      bob += 1
+    end
   end
+  puts "#{alice} #{bob}"
 end
-
-puts "#{alice} #{bob}"
